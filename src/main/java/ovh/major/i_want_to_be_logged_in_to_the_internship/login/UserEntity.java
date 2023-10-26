658be5569abb99981df52cbb5465982305e082ff
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-10-26 14:25:38 +0200

    added UserEntity class

---
+++ src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/login/UserEntity.java
@@ -0,0 +1,32 @@
+package ovh.major.i_want_to_be_logged_in_to_the_internship.login;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @NotBlank
+    @Column(unique = true)
+    private String username;
+
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    @Email
+    @Column(unique = true)
+    private String email;
+ }
