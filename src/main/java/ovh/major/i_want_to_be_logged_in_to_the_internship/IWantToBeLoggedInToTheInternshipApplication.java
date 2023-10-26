c5812833ae678c6475aef0a01c1b3af94b03892c
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-10-26 14:00:37 +0200

    first commit - empty project

---
+++ src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/IWantToBeLoggedInToTheInternshipApplication.java
@@ -0,0 +1,13 @@
+package ovh.major.i_want_to_be_logged_in_to_the_internship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IWantToBeLoggedInToTheInternshipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IWantToBeLoggedInToTheInternshipApplication.class, args);
+    }
+
+}
