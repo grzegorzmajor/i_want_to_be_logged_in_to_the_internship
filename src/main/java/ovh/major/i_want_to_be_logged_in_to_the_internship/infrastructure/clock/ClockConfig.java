9cf31c058b12d0751a69270f374c29d213dc60de
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-10-27 19:56:04 +0200

    added clock configuration class for JWT

---
+++ src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/infrastructure/clock/ClockConfig.java
@@ -0,0 +1,15 @@
+package ovh.major.i_want_to_be_logged_in_to_the_internship.infrastructure.clock;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Clock;
+
+@Configuration
+public class ClockConfig {
+
+    @Bean
+    Clock clock() {
+        return Clock.systemUTC();
+    }
+}
