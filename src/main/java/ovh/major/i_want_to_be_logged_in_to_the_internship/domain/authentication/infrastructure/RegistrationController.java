56278c010d4ce9384c7aa7cc821f040121bea940
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-10-27 19:58:23 +0200

    added empty yet Rest Controllers classes

---
+++ src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/domain/authentication/infrastructure/RegistrationController.java
@@ -0,0 +1,11 @@
+package ovh.major.i_want_to_be_logged_in_to_the_internship.domain.authentication.infrastructure;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/register")
+class RegistrationController {
+}
