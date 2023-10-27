8729c35386c5cfb069fffdb663ef51dc0e82f9f5
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-10-27 19:37:13 +0200

    moved, added declaration of first method findByUsername()

--- src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/domain/authentication/infrastructure/UserRepository.java
+++ src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/domain/authentication/infrastructure/UserRepository.java
@@ -1,4 +1,4 @@
-package ovh.major.i_want_to_be_logged_in_to_the_internship.authentication;
+package ovh.major.i_want_to_be_logged_in_to_the_internship.domain.authentication.infrastructure;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
