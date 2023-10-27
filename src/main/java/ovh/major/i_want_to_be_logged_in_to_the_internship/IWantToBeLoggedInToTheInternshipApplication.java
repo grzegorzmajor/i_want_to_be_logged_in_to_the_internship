416e7a981926e47060f3e3768016a86ca4e8cf5d
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-10-27 19:55:22 +0200

    added @EnableConfigurationProperties annotation

--- src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/IWantToBeLoggedInToTheInternshipApplication.java
+++ src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/IWantToBeLoggedInToTheInternshipApplication.java
@@ -2,8 +2,11 @@ package ovh.major.i_want_to_be_logged_in_to_the_internship;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import ovh.major.i_want_to_be_logged_in_to_the_internship.domain.authentication.configurations.JwtConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(value = {JwtConfigurationProperties.class})
 public class IWantToBeLoggedInToTheInternshipApplication {
 
     public static void main(String[] args) {
