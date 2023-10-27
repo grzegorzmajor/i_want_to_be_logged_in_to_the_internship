6030fdcc482cf438128e0de582d1b93e9e5ca4a6
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-10-27 19:48:37 +0200

    added security configuration class

---
+++ src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/infrastructure/security/SecurityConfig.java
@@ -0,0 +1,63 @@
+package ovh.major.i_want_to_be_logged_in_to_the_internship.infrastructure.security;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import ovh.major.i_want_to_be_logged_in_to_the_internship.domain.authentication.LoginFacade;
+
+@Configuration
+@AllArgsConstructor
+class SecurityConfig {
+
+    private final JwtAuthTokenFilter jwtAuthTokenFilter;
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(LoginFacade loginFacade) {
+        return new LoginUserDetailsService(loginFacade);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+        httpSecurity.authorizeHttpRequests(request -> request
+                .requestMatchers(
+                        "/login/**"
+                ).permitAll()
+//                .requestMatchers(HttpMethod.GET,
+//                        ""
+//                ).permitAll()
+                .anyRequest().authenticated()
+        );
+        httpSecurity.headers(header -> header
+                .frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)
+        );
+        httpSecurity.httpBasic(AbstractHttpConfigurer::disable);
+        httpSecurity.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        httpSecurity.exceptionHandling(Customizer.withDefaults());
+        httpSecurity.addFilterBefore(jwtAuthTokenFilter, UsernamePasswordAuthenticationFilter.class);
+        return httpSecurity.build();
+    }
+
+}
