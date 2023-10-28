c36e8d8e48360994e0c9f01ccea178fd6560202a
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-10-28 18:06:28 +0200

    moved, changed expiration time from days to seconds

--- src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/infrastructure/jwt/JwtConfigurationProperties.java
+++ src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/infrastructure/jwt/JwtConfigurationProperties.java
@@ -1,11 +1,11 @@
-package ovh.major.i_want_to_be_logged_in_to_the_internship.domain.authentication.configurations;
+package ovh.major.i_want_to_be_logged_in_to_the_internship.infrastructure.jwt;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(value = "auth.jwt")
 public record JwtConfigurationProperties(
         String secret,
-        long expirationDays,
+        long expirationSeconds,
         String issuer
 ) {
 }
