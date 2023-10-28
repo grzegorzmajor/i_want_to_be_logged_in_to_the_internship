1be267f4af2fc99a918582a3ce4b8a6b9c3ea746
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-10-28 18:06:52 +0200

    moved

--- src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/infrastructure/authentication/LoginController.java
+++ src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/infrastructure/authentication/LoginController.java
@@ -1,8 +1,9 @@
-package ovh.major.i_want_to_be_logged_in_to_the_internship.domain.authentication.infrastructure;
+package ovh.major.i_want_to_be_logged_in_to_the_internship.infrastructure.authentication;
 
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ovh.major.i_want_to_be_logged_in_to_the_internship.infrastructure.jwt.JwtAuthenticator;
 
 @RestController
 @AllArgsConstructor
--- src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/infrastructure/authentication/RegistrationController.java
+++ src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/infrastructure/authentication/RegistrationController.java
@@ -1,4 +1,4 @@
-package ovh.major.i_want_to_be_logged_in_to_the_internship.domain.authentication.infrastructure;
+package ovh.major.i_want_to_be_logged_in_to_the_internship.infrastructure.authentication;
 
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
