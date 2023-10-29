4ea3ded39b9ab07f1e9570dd7ef5c5dc92bc15e1
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-10-29 11:37:50 +0100

    refactored, added sending email confirmation

--- src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/domain/authentication/infrastructure/RegistrationService.java
+++ src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/domain/authentication/infrastructure/RegistrationService.java
@@ -1,17 +1,24 @@
 package ovh.major.i_want_to_be_logged_in_to_the_internship.domain.authentication.infrastructure;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ovh.major.i_want_to_be_logged_in_to_the_internship.domain.authentication.dto.RegistrationResultDto;
+import ovh.major.i_want_to_be_logged_in_to_the_internship.domain.authentication.dto.UserForEmailDto;
 import ovh.major.i_want_to_be_logged_in_to_the_internship.domain.authentication.dto.UserRegisterRequestDto;
+import ovh.major.i_want_to_be_logged_in_to_the_internship.domain.email.sender.EmailSenderFacade;
 
 @Service
 class RegistrationService {
 
     UserRepository userRepository;
+    EmailSenderFacade emailSenderFacade;
 
+    @Transactional
     public RegistrationResultDto registerUser(UserRegisterRequestDto userRegisterRequestDto) {
         UserEntity userEntity = UserMappers.fromUserRegisterRequestDtoToUserEntity(userRegisterRequestDto);
-        UserEntity saveResult = userRepository.saveUser(userEntity);
-        return UserMappers.fromUserEntityToRegistrationResultDto(saveResult);
+        UserEntity savedResult = userRepository.saveUser(userEntity);
+        UserForEmailDto userForEmailDto = UserMappers.fromUserEntitytoUserForEmailDto(savedResult);
+        emailSenderFacade.sendConfirmationEmail(userForEmailDto);
+        return UserMappers.fromUserEntityToRegistrationResultDto(savedResult);
     }
 }
