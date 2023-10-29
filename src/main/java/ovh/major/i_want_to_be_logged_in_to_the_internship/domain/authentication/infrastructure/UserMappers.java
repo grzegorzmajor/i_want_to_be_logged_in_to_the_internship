c3d62bee7f6c2c2a61c346a95fe52d4279cfb002
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-10-29 11:29:20 +0100

    renamed from UserMapper and added new mappers

---
+++ src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/domain/authentication/infrastructure/UserMappers.java
@@ -0,0 +1,32 @@
+package ovh.major.i_want_to_be_logged_in_to_the_internship.domain.authentication.infrastructure;
+
+import ovh.major.i_want_to_be_logged_in_to_the_internship.domain.authentication.dto.RegistrationResultDto;
+import ovh.major.i_want_to_be_logged_in_to_the_internship.domain.authentication.dto.UserDto;
+import ovh.major.i_want_to_be_logged_in_to_the_internship.domain.authentication.dto.UserRegisterRequestDto;
+
+class UserMappers {
+    public static UserDto fromUserEntitytoUserDto(UserEntity userEntity) {
+        return UserDto.builder()
+                .username(userEntity.getUsername())
+                .password(userEntity.getPassword())
+                .build();
+    }
+
+    public static UserEntity fromUserRegisterRequestDtoToUserEntity(UserRegisterRequestDto userRegisterRequestDto) {
+        return UserEntity.builder()
+                .username(userRegisterRequestDto.username())
+                .password(userRegisterRequestDto.password())
+                .email(userRegisterRequestDto.email())
+                .emailAuthenticated(false)
+                .build();
+    }
+    public static RegistrationResultDto fromUserEntityToRegistrationResultDto(UserEntity userEntity) {
+        return RegistrationResultDto.builder()
+                .id(userEntity.getId())
+                .username(userEntity.getUsername())
+                .email(userEntity.getEmail())
+                .registered(true)
+                .build();
+
+    }
+}
