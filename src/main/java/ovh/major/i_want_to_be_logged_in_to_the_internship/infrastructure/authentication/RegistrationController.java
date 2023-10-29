648c55a1c690eb25e0f626f302c168960d8a1d74
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-10-29 11:27:50 +0100

    added registration rest controller class

--- src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/infrastructure/authentication/RegistrationController.java
+++ src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/infrastructure/authentication/RegistrationController.java
@@ -1,11 +1,34 @@
 package ovh.major.i_want_to_be_logged_in_to_the_internship.infrastructure.authentication;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ovh.major.i_want_to_be_logged_in_to_the_internship.domain.authentication.dto.RegistrationResultDto;
+import ovh.major.i_want_to_be_logged_in_to_the_internship.domain.authentication.dto.UserRegisterRequestDto;
+import ovh.major.i_want_to_be_logged_in_to_the_internship.domain.authentication.infrastructure.AuthorizationFacade;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/register")
 class RegistrationController {
+
+    private final PasswordEncoder bCryptPasswordEncoder;
+    private final AuthorizationFacade authorizationFacade;
+
+    @PostMapping
+    public ResponseEntity<RegistrationResultDto> registerUser(@RequestBody UserRegisterRequestDto userRegisterRequestDto) {
+        String encodedPassword = bCryptPasswordEncoder.encode(userRegisterRequestDto.password());
+        RegistrationResultDto registrationResultDto = authorizationFacade.registerUser(
+                new UserRegisterRequestDto(
+                        userRegisterRequestDto.username(),
+                        encodedPassword,
+                        userRegisterRequestDto.email()));
+        return ResponseEntity.status(HttpStatus.CREATED).body(registrationResultDto);
+
+    }
 }
