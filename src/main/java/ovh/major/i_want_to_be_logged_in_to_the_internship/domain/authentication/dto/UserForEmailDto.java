459f9f830dc7a85b49dc93e15ab63bb360ebdb8e
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-10-29 11:36:32 +0100

    added builder

--- src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/domain/authentication/dto/UserForEmailDto.java
+++ src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/domain/authentication/dto/UserForEmailDto.java
@@ -1,7 +1,10 @@
 package ovh.major.i_want_to_be_logged_in_to_the_internship.domain.authentication.dto;
 
+import lombok.Builder;
+
 import javax.validation.constraints.NotBlank;
 
+@Builder
 public record UserForEmailDto(
         @NotBlank(message = "Username must be not blank!")
         String username,
