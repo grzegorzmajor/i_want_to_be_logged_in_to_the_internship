50b214cdf7bb8e0075104f79cdd9ebcef62b12d9
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-10-29 11:27:13 +0100

    added new DTO for registration result

---
+++ src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/domain/authentication/dto/RegistrationResultDto.java
@@ -0,0 +1,11 @@
+package ovh.major.i_want_to_be_logged_in_to_the_internship.domain.authentication.dto;
+
+import lombok.Builder;
+
+@Builder
+public record RegistrationResultDto(
+    Integer id,
+    String username,
+    String email,
+    boolean  registered
+) {}
