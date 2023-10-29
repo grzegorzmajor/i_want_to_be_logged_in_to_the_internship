053afb652955b0fb6729fc5250acd71891bddf81
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-10-29 11:25:21 +0100

    refactored

--- src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/domain/email/confirmer/ConfirmingController.java
+++ src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/domain/email/confirmer/ConfirmingController.java
@@ -3,19 +3,19 @@ package ovh.major.i_want_to_be_logged_in_to_the_internship.domain.email.confirme
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import ovh.major.i_want_to_be_logged_in_to_the_internship.domain.authentication.infrastructure.LoginFacade;
+import ovh.major.i_want_to_be_logged_in_to_the_internship.domain.authentication.infrastructure.AuthorizationFacade;
 import ovh.major.i_want_to_be_logged_in_to_the_internship.domain.email.jwt.JwtForEmailTokenProvider;
 
 @RestController
 class ConfirmingController {
 
     JwtForEmailTokenProvider tokenProvider;
-    LoginFacade loginFacade;
+    AuthorizationFacade authorizationFacade;
 
     @GetMapping("/confirm/{token}")
     public void confirm(@PathVariable String token) {
         String username = tokenProvider.getUsernameFromToken(token);
-        loginFacade.emailConfirmation(username);
+        authorizationFacade.emailConfirmation(username);
     }
 
 }
