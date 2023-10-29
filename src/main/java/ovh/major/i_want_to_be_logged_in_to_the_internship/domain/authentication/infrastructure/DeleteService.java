87e01fe2b8b8192515c9174459cb16246381a312
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-10-29 12:29:15 +0100

    added empty classes for future functionalities

---
+++ src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/domain/authentication/infrastructure/DeleteService.java
@@ -0,0 +1,7 @@
+package ovh.major.i_want_to_be_logged_in_to_the_internship.domain.authentication.infrastructure;
+
+import org.springframework.stereotype.Service;
+
+@Service
+class DeleteService {
+}
