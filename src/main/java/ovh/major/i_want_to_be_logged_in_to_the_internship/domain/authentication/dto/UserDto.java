f810d42a03754c3b11ec2b81186665ba17c6a5ab
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-10-29 07:36:24 +0100

    added builder

--- src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/domain/authentication/dto/UserDto.java
+++ src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/domain/authentication/dto/UserDto.java
@@ -1,5 +1,8 @@
 package ovh.major.i_want_to_be_logged_in_to_the_internship.domain.authentication.dto;
 
+import lombok.Builder;
+
+@Builder
 public record UserDto(
         String username,
         String password
