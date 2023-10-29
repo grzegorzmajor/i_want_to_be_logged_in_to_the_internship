15835401e365f9c97f33b7a118a802fa76df0050
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-10-29 07:37:19 +0100

    deleted unnecessary annotation

--- src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/IWantToBeLoggedInToTheInternshipApplication.java
+++ src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/IWantToBeLoggedInToTheInternshipApplication.java
@@ -2,11 +2,8 @@ package ovh.major.i_want_to_be_logged_in_to_the_internship;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import ovh.major.i_want_to_be_logged_in_to_the_internship.infrastructure.jwt.JwtConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(value = {JwtConfigurationProperties.class})
 public class IWantToBeLoggedInToTheInternshipApplication {
 
     public static void main(String[] args) {
