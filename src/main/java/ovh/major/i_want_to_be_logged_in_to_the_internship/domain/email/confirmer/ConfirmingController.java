150b054619d29a4a1aa7091eec1bc2ae6505f006
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-10-29 07:49:23 +0100

    added rest controller for email confirmation endpoint

---
+++ src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/domain/email/confirmer/ConfirmingController.java
@@ -0,0 +1,21 @@
+package ovh.major.i_want_to_be_logged_in_to_the_internship.domain.email.confirmer;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import ovh.major.i_want_to_be_logged_in_to_the_internship.domain.authentication.infrastructure.LoginFacade;
+import ovh.major.i_want_to_be_logged_in_to_the_internship.domain.email.jwt.JwtForEmailTokenProvider;
+
+@RestController
+class ConfirmingController {
+
+    JwtForEmailTokenProvider tokenProvider;
+    LoginFacade loginFacade;
+
+    @GetMapping("/confirm/{token}")
+    public void confirm(@PathVariable String token) {
+        String username = tokenProvider.getUsernameFromToken(token);
+        loginFacade.emailConfirmation(username);
+    }
+
+}
