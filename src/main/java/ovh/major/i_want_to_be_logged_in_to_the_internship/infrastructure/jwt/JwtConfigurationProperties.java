da5873bdc9c75d8e9c04810738e2fee70dad7c3a
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-10-29 07:38:35 +0100

    changed annotation for scope changing

--- src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/infrastructure/jwt/JwtConfigurationProperties.java
+++ src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/infrastructure/jwt/JwtConfigurationProperties.java
@@ -1,11 +1,15 @@
 package ovh.major.i_want_to_be_logged_in_to_the_internship.infrastructure.jwt;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
-@ConfigurationProperties(value = "auth.jwt")
-public record JwtConfigurationProperties(
+@Component
+record JwtConfigurationProperties(
+        @Value("auth.jwt.auth.secret")
         String secret,
+        @Value("auth.jwt.auth.expirationSeconds")
         long expirationSeconds,
+        @Value("auth.jwt.auth.issuer")
         String issuer
 ) {
 }
