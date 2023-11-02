ecff66cc0208f57bc8c70be3dc93cc0f42b52dd3
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-11-02 20:41:17 +0100

    added login controller - implementation authentication and token generating

--- src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/infrastructure/authentication/LoginController.java
+++ src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/infrastructure/authentication/LoginController.java
@@ -1,9 +1,16 @@
 package ovh.major.i_want_to_be_logged_in_to_the_internship.infrastructure.authentication;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ovh.major.i_want_to_be_logged_in_to_the_internship.domain.authentication.dto.TokenResponseDto;
+import ovh.major.i_want_to_be_logged_in_to_the_internship.domain.authentication.dto.UserLoginRequestDto;
 import ovh.major.i_want_to_be_logged_in_to_the_internship.infrastructure.jwt.JwtAuthenticator;
+
+import javax.validation.Valid;
 
 @RestController
 @AllArgsConstructor
@@ -11,5 +18,12 @@ import ovh.major.i_want_to_be_logged_in_to_the_internship.infrastructure.jwt.Jwt
 class LoginController {
 
     private final JwtAuthenticator jwtAuthenticatorFacade;
+
+    @PostMapping
+    public ResponseEntity<TokenResponseDto> authenticateAndGenerateToken(@RequestBody @Valid UserLoginRequestDto tokenRequestDto) {
+        final TokenResponseDto tokenResponseDto = jwtAuthenticatorFacade.authenticateAndGenerateToken(tokenRequestDto);
+        return ResponseEntity.ok(tokenResponseDto);
+    }
+
 
 }
