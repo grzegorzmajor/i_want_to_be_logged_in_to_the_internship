4311487e005580d30247ed25abde79706490e17e
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-11-02 20:35:52 +0100

    changed constructor from lombok annotation to real implementation because @Qualifier needed, fixed bug

--- src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/domain/email/jwt/JwtForEmailTokenProvider.java
+++ src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/domain/email/jwt/JwtForEmailTokenProvider.java
@@ -4,24 +4,30 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.auth0.jwt.interfaces.JWTVerifier;
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import java.time.*;
 
-@AllArgsConstructor
 @Component
 public class JwtForEmailTokenProvider {
+    public JwtForEmailTokenProvider(
+            @Qualifier("auth.jwt.email-ovh.major.i_want_to_be_logged_in_to_the_internship.domain.email.jwt.JwtForEmailConfigurationProperties")
+            JwtForEmailConfigurationProperties properties,
+            Clock clock) {
+        this.properties = properties;
+        this.clock = clock;
+    }
 
     private final JwtForEmailConfigurationProperties properties;
     private final Clock clock;
 
     public String generateToken(String username) {
-        String secretKey = properties.secret();
+        String secretKey = properties.getSecret();
         Algorithm algorithm = Algorithm.HMAC256(secretKey);
         Instant now = LocalDateTime.now(clock).toInstant(ZoneOffset.UTC);
-        Instant expiresAt = now.plus(Duration.ofSeconds(properties.expirationSeconds()));
-        String issuer = properties.issuer();
+        Instant expiresAt = now.plus(Duration.ofSeconds(properties.expirationSeconds));
+        String issuer = properties.getIssuer();
         return JWT.create()
                 .withSubject(username)
                 .withIssuedAt(now)
@@ -31,7 +37,7 @@ public class JwtForEmailTokenProvider {
     }
 
     public String getUsernameFromToken(String token) {
-        String secretKey = properties.secret();
+        String secretKey = properties.getSecret();
         Algorithm algorithm = Algorithm.HMAC256(secretKey);
         JWTVerifier verifier = JWT.require(algorithm)
                 .build();
