8a345048070c2077a893e9701d04b57b49b027d8
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-11-02 20:23:37 +0100

    added exception messages enum class

---
+++ src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/domain/authentication/infrastructure/exceptions/ExceptionMessages.java
@@ -0,0 +1,19 @@
+package ovh.major.i_want_to_be_logged_in_to_the_internship.domain.authentication.infrastructure.exceptions;
+
+public enum ExceptionMessages {
+    USER_EXISTS("A user with that name or e-mail exists!"),
+    USER_NOT_FOUND("User not found!"),
+    EMAIL_NOT_CONFIRMED("Email is not confirmed!"),
+    BAD_CREDENTIALS("Bad Credentials");
+
+    private String s;
+
+    ExceptionMessages(String s) {
+        this.s = s;
+    }
+
+    @Override
+    public String toString() {
+        return s;
+    }
+}
