94ae3eb9e5e7290ce079f18c2fbd7b4af7c20cf7
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-11-02 22:50:06 +0100

    deleted unnecessary import

--- src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/infrastructure/authentication/EmailConfirmingController.java
+++ src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/infrastructure/authentication/EmailConfirmingController.java
@@ -1,7 +1,6 @@
 package ovh.major.i_want_to_be_logged_in_to_the_internship.infrastructure.authentication;
 
 import com.auth0.jwt.exceptions.TokenExpiredException;
-import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
