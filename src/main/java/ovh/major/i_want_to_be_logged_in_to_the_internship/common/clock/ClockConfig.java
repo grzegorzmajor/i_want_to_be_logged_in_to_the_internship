3fe4eaf6add8c76bf915c866805b63cd08d9ada4
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-11-02 20:15:46 +0100

    moved

--- src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/common/clock/ClockConfig.java
+++ src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/common/clock/ClockConfig.java
@@ -1,13 +1,10 @@
-package ovh.major.i_want_to_be_logged_in_to_the_internship.infrastructure.clock;
-
+package ovh.major.i_want_to_be_logged_in_to_the_internship.common.clock;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.time.Clock;
-
 @Configuration
 class ClockConfig {
-
     @Bean
     Clock clock() {
         return Clock.systemUTC();
