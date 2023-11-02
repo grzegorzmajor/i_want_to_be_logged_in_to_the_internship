8b2a7572e388b045784faaf0d0932407650cc846
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-11-02 20:22:42 +0100

    refactored - method emailConfirmation extracted to other class, added exceptions added @AllArgsConstructor

--- src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/domain/authentication/infrastructure/LoginService.java
+++ src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/domain/authentication/infrastructure/LoginService.java
@@ -1,22 +1,30 @@
 package ovh.major.i_want_to_be_logged_in_to_the_internship.domain.authentication.infrastructure;
 
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import ovh.major.i_want_to_be_logged_in_to_the_internship.domain.authentication.dto.UserDto;
+import ovh.major.i_want_to_be_logged_in_to_the_internship.domain.authentication.infrastructure.exceptions.EmailNotConfirmedException;
+
+import static ovh.major.i_want_to_be_logged_in_to_the_internship.domain.authentication.infrastructure.exceptions.ExceptionMessages.USER_NOT_FOUND;
 
 @Service
+@AllArgsConstructor
 class LoginService {
 
     UserRepository repository;
 
-    @Transactional
-    public void emailConfirmation(String username) {
-        repository.emailAuthenticateByUsername(username);
-    }
-
     public UserDto findByUsername(String username) {
-        UserEntity userEntity = repository.findByUsername(username);
-        UserDto userDto = UserMappers.fromUserEntitytoUserDto(userEntity);
-        return userDto;
+        UserEntity userEntity;
+        try {
+            userEntity = repository.findByUsername(username);
+        }
+        catch (RuntimeException e) {
+            throw new BadCredentialsException(USER_NOT_FOUND.toString());
+        }
+        if (!userEntity.isEmailAuthenticated()) {
+            throw new EmailNotConfirmedException();
+        }
+        return UserMappers.fromUserEntitytoUserDto(userEntity);
     }
 }
