28c9940aacd7a7ab0aac54cbf3d9ca657a3a8788
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-11-02 20:30:29 +0100

    deleted empty test method

--- src/test/java/ovh/major/i_want_to_be_logged_in_to_the_internship/IWantToBeLoggedInToTheInternshipApplicationTests.java
+++ src/test/java/ovh/major/i_want_to_be_logged_in_to_the_internship/IWantToBeLoggedInToTheInternshipApplicationTests.java
@@ -1,13 +1,8 @@
 package ovh.major.i_want_to_be_logged_in_to_the_internship;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class IWantToBeLoggedInToTheInternshipApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
