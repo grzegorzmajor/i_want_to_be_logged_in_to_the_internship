325fc103e2b9eaa13b0a51dda4b044ccc46069f8
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-11-02 20:25:51 +0100

    changed implementation of email sender configuration

--- src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/domain/email/sender/EmailConfigurationProperties.java
+++ src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/domain/email/sender/EmailConfigurationProperties.java
@@ -1,14 +1,20 @@
 package ovh.major.i_want_to_be_logged_in_to_the_internship.domain.email.sender;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
-record EmailConfigurationProperties(
-        @Value("${spring.mail.host}")
-        String host,
-        @Value("${spring.mail.port}")
-        int port,
-        @Value("${spring.mail.username}")
-        String username,
-        @Value("${spring.mail.password}")
-        String password
-) {}
+@Configuration
+@PropertySource("classpath:application.yml")
+@ConfigurationProperties(value = "spring.mail")
+@Getter
+@Setter
+class EmailConfigurationProperties {
+        String host;
+        int port;
+        String username;
+        String password;
+}
