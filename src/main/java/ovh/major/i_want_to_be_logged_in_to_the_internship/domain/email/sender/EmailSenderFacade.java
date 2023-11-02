3727da7f4169858a9f01f9e9f15351ef2f9b6eea
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-11-02 20:28:11 +0100

    refactored, added missed annotations

--- src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/domain/email/sender/EmailSenderFacade.java
+++ src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/domain/email/sender/EmailSenderFacade.java
@@ -1,30 +1,35 @@
 package ovh.major.i_want_to_be_logged_in_to_the_internship.domain.email.sender;
 
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
 import ovh.major.i_want_to_be_logged_in_to_the_internship.domain.authentication.dto.UserForEmailDto;
 import ovh.major.i_want_to_be_logged_in_to_the_internship.domain.email.jwt.JwtForEmailTokenProvider;
 
+@Component
+@AllArgsConstructor
 public class EmailSenderFacade {
 
     EmailSenderService emailSenderService;
     JwtForEmailTokenProvider tokenProvider;
 
-    private final static String subject = "Email confirmation from The Internship Application.";
+    private final static String EMAIL_CONFIRMATION_SUBJECT = "Email confirmation from The Internship Application.";
+    private final static String APPLICATION_LINK = "http://localhost:8888/";
 
 
     public void sendConfirmationEmail(UserForEmailDto user) {
         emailSenderService.sendEmail(
                 user.email(),
-                subject,
-                getConfirmationEmailWithLinkContent(user.username())
+                EMAIL_CONFIRMATION_SUBJECT,
+                getConfirmationEmailContentWithLink(user.username())
         );
     }
 
-    private String getConfirmationEmailWithLinkContent(String username) {
+    private String getConfirmationEmailContentWithLink(String username) {
         return EmailWithLinkContent.builder()
                 .title("Email confirmation!")
                 .textBeforeLink("Hi, " + username + "! You have registered in The Internship Application. If you registered," +
                         "you must confirm your email. Click on the link below or copy and paste it into your browser.")
-                .link("https://localhost:76543/confirm/" + tokenProvider.generateToken(username))
+                .link(APPLICATION_LINK + "confirm/" + tokenProvider.generateToken(username))
                 .textAfterLink("The link will expire after 5 minutes!")
                 .build()
                 .toString();
