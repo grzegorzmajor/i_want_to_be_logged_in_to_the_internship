49aeb0d5c447280afdf8d6a807b36ed49700339a
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-11-03 17:03:24 +0100

    added deleteByUsername method

--- src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/domain/authentication/infrastructure/DeleteService.java
+++ src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/domain/authentication/infrastructure/DeleteService.java
@@ -1,8 +1,23 @@
 package ovh.major.i_want_to_be_logged_in_to_the_internship.domain.authentication.infrastructure;
 
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import ovh.major.i_want_to_be_logged_in_to_the_internship.domain.authentication.infrastructure.exceptions.ExceptionMessages;
 
 @Service
+@AllArgsConstructor
 class DeleteService {
-    //todo please implement that
+
+    UserRepository repository;
+
+    void deleteByUsername(String username){
+        UserEntity user = repository.findByUsername(username);
+        if (!user.getUsername()
+                .equals(username)) {
+            throw new UsernameNotFoundException(ExceptionMessages.USER_NOT_FOUND.toString());
+        }
+        repository.deleteUserByUsername(username);
+    }
+
 }
