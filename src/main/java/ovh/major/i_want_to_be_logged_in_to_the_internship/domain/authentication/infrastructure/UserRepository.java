373bae890b5be8bec3565dfbffdcf5c2376e3418
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-11-03 21:23:30 +0100

    added new methods exists..., changed update username from byId to byUsername

--- src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/domain/authentication/infrastructure/UserRepository.java
+++ src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/domain/authentication/infrastructure/UserRepository.java
@@ -22,8 +22,8 @@ interface UserRepository extends JpaRepository<UserEntity, Integer> {
     Integer findUserIdByUsername(String username);
 
     @Modifying
-    @Query("UPDATE UserEntity user SET user.username = :username WHERE user.id = :id")
-    void updateUsernameById(Integer id, String username);
+    @Query("UPDATE UserEntity user SET user.username = :newUsername WHERE user.username = :oldUsername")
+    void updateUsernameByUsername(String oldUsername, String newUsername);
 
     @Modifying
     @Query("UPDATE UserEntity user SET user.email = :email, user.emailAuthenticated = false WHERE user.username = :username")
@@ -40,4 +40,9 @@ interface UserRepository extends JpaRepository<UserEntity, Integer> {
     @Query("SELECT CASE WHEN COUNT(user) > 0 THEN true ELSE false END FROM UserEntity user WHERE user.username = :username OR user.email = :email")
     boolean existsUserEntityByUsernameOrEmail(String username, String email);
 
+    @Query("SELECT CASE WHEN COUNT(user) > 0 THEN true ELSE false END FROM UserEntity user WHERE user.username = :username")
+    boolean existsUserEntityByUsername(String username);
+
+    @Query("SELECT CASE WHEN COUNT(user) > 0 THEN true ELSE false END FROM UserEntity user WHERE user.email = :email")
+    boolean existsUserEntityByEmail(String email);
 }
