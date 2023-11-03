3e940a8cf018d5f04045b2c830ca5e93c295ab37
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-11-03 17:01:46 +0100

    added deleteUser method

--- src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/domain/authentication/infrastructure/AuthorizationFacade.java
+++ src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/domain/authentication/infrastructure/AuthorizationFacade.java
@@ -3,6 +3,7 @@ package ovh.major.i_want_to_be_logged_in_to_the_internship.domain.authentication
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import ovh.major.i_want_to_be_logged_in_to_the_internship.domain.authentication.dto.RegistrationResultDto;
 import ovh.major.i_want_to_be_logged_in_to_the_internship.domain.authentication.dto.UserDto;
 import ovh.major.i_want_to_be_logged_in_to_the_internship.domain.authentication.dto.UserRegisterRequestDto;
@@ -13,6 +14,7 @@ public class AuthorizationFacade {
     LoginService loginService;
     EmailConfirmationService emailConfirmationService;
     RegistrationService registrationService;
+    DeleteService deleteService;
 
     public void confirmEmail(String username) {
         emailConfirmationService.confirm(username);
@@ -24,5 +26,10 @@ public class AuthorizationFacade {
 
     public RegistrationResultDto registerUser(UserRegisterRequestDto userRegisterRequestDto) {
         return registrationService.registerUser(userRegisterRequestDto);
+    }
+
+    @Transactional
+    public void deleteUser(String username) {
+        deleteService.deleteByUsername(username);
     }
 }
