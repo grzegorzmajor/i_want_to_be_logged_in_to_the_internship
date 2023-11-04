06911fee7a6428ee132f8377731bdd7dcfb7e606
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-11-04 21:15:05 +0100

    deleted unnecessary test class

--- src/test/java/ovh/major/i_want_to_be_logged_in_to_the_internship/IWantToBeLoggedInToTheInternshipApplicationTests.java
+++
@@ -1,8 +0,0 @@
-package ovh.major.i_want_to_be_logged_in_to_the_internship;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class IWantToBeLoggedInToTheInternshipApplicationTests {
-
-}
