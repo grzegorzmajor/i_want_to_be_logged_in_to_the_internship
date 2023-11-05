833bcc08bd2a37eecacbded3e85e02b35823472c
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-11-05 11:46:30 +0100

    refactored, added new method

--- src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/domain/authentication/infrastructure/LoginService.java
+++ src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/domain/authentication/infrastructure/LoginService.java
@@ -3,7 +3,6 @@ package ovh.major.i_want_to_be_logged_in_to_the_internship.domain.authentication
 import lombok.AllArgsConstructor;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.stereotype.Service;
-import ovh.major.i_want_to_be_logged_in_to_the_internship.domain.authentication.dto.UserDto;
 import ovh.major.i_want_to_be_logged_in_to_the_internship.domain.authentication.infrastructure.exceptions.EmailNotConfirmedException;
 
 import static ovh.major.i_want_to_be_logged_in_to_the_internship.domain.authentication.infrastructure.exceptions.ExceptionMessages.USER_NOT_FOUND;
@@ -12,19 +11,21 @@ import static ovh.major.i_want_to_be_logged_in_to_the_internship.domain.authenti
 @AllArgsConstructor
 class LoginService {
 
-    UserRepository repository;
+    private final UserRepository repository;
 
-    public UserDto findByUsername(String username) {
-        UserEntity userEntity;
-        try {
-            userEntity = repository.findByUsername(username);
-        }
-        catch (RuntimeException e) {
-            throw new BadCredentialsException(USER_NOT_FOUND.toString());
-        }
+    public UserEntity findByUsername(String username) {
+        UserEntity userEntity = repository
+                .findByUsername(username)
+                .orElseThrow(() -> new BadCredentialsException(USER_NOT_FOUND.toString()));
         if (!userEntity.isEmailAuthenticated()) {
             throw new EmailNotConfirmedException();
         }
-        return UserMappers.fromUserEntityToUserDto(userEntity);
+        return userEntity;
+    }
+
+    public UserEntity findById(Integer id) {
+        return repository
+                .findById(id)
+                .orElseThrow(() -> new BadCredentialsException(USER_NOT_FOUND.toString()));
     }
 }
