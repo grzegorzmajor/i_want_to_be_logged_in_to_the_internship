6e751151129425b6701d161761115878b305061f
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-11-05 21:09:55 +0100

    deleted unnecessary import

--- src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/domain/email/sender/EmailConfigurationProperties.java
+++ src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/domain/email/sender/EmailConfigurationProperties.java
@@ -2,7 +2,6 @@ package ovh.major.i_want_to_be_logged_in_to_the_internship.domain.email.sender;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
