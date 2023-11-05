478e6b39d0cba9c97e9e714da5752458fb8f6d32
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-11-05 11:47:56 +0100

    deleted sending email with update information

--- src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/domain/authentication/infrastructure/UpdateService.java
+++ src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/domain/authentication/infrastructure/UpdateService.java
@@ -3,9 +3,7 @@ package ovh.major.i_want_to_be_logged_in_to_the_internship.domain.authentication
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import ovh.major.i_want_to_be_logged_in_to_the_internship.domain.authentication.dto.UserForEmailDto;
 import ovh.major.i_want_to_be_logged_in_to_the_internship.domain.authentication.infrastructure.exceptions.DuplicateCredentialsException;
-import ovh.major.i_want_to_be_logged_in_to_the_internship.domain.email.sender.EmailSenderFacade;
 
 import static ovh.major.i_want_to_be_logged_in_to_the_internship.domain.authentication.infrastructure.exceptions.ExceptionMessages.USER_NOT_FOUND;
 
@@ -14,7 +12,6 @@ import static ovh.major.i_want_to_be_logged_in_to_the_internship.domain.authenti
 class UpdateService {
 
     UserRepository repository;
-    EmailSenderFacade emailSenderFacade;
 
     void updateUsernameByOldUsername(String oldUsername, String newUsername) {
         throwWhenUsernameNoExistInDb(oldUsername);
@@ -22,9 +19,6 @@ class UpdateService {
             throw new DuplicateCredentialsException();
         }
         repository.updateUsernameByUsername(oldUsername, newUsername);
-        UserEntity updateResult = repository.findByUsername(newUsername);
-        UserForEmailDto userForEmailDto = UserMappers.fromUserEntityToUserForEmailDto(updateResult);
-        emailSenderFacade.sendSecurityInformationEmail(userForEmailDto, "Your username has bean changed!");
     }
 
     void updateEmailByUsername(String username, String newEmail) {
@@ -33,17 +27,11 @@ class UpdateService {
             throw new DuplicateCredentialsException();
         }
         repository.updateEmailByUsername(username,newEmail);
-        UserEntity updateResult = repository.findByUsername(username);
-        UserForEmailDto userForEmailDto = UserMappers.fromUserEntityToUserForEmailDto(updateResult);
-        emailSenderFacade.sendConfirmationEmail(userForEmailDto);
     }
 
     void updatePasswordByUsername(String username, String password) {
         throwWhenUsernameNoExistInDb(username);
         repository.updatePasswordByUsername(username,password);
-        UserEntity updateResult = repository.findByUsername(username);
-        UserForEmailDto userForEmailDto = UserMappers.fromUserEntityToUserForEmailDto(updateResult);
-        emailSenderFacade.sendSecurityInformationEmail(userForEmailDto, "Your password has bean changed!");
     }
 
     private void throwWhenUsernameNoExistInDb(String oldUsername) {
