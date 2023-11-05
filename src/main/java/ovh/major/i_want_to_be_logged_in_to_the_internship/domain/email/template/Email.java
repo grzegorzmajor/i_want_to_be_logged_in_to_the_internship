3754cfc4f73bf1dc1b392225be1bc785a9a48c1f
Grzegorz Major <116442236+grzegorzmajor@users.noreply.github.com>
2023-11-05 14:07:02 +0100

    renamed

--- src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/domain/email/template/Email.java
+++ src/main/java/ovh/major/i_want_to_be_logged_in_to_the_internship/domain/email/template/Email.java
@@ -3,7 +3,7 @@ package ovh.major.i_want_to_be_logged_in_to_the_internship.domain.email.template
 import lombok.Builder;
 
 @Builder
-public record EmailTemplate(
+public record Email(
         String subject,
         String body
 ) { }
